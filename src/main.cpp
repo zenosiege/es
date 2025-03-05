54a9a9268f64adca4aa2c0f17aa146093e1c56be
Pantileev Danila <neeta7love@gmail.com>
2025-03-05 18:43:34 +0300

    Minor fixes

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main() {
 
     rcc_periph_clock_enable(RCC_GPIOD);
 
-    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_MODE, GPIO15);
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
     while (true) {
         gpio_toggle(GPIOD, GPIO15);
         for (volatile uint32_t i = 0; i<2'000'000; ++i);
