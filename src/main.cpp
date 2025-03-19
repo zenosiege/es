b7b7071d1daf00994cb9fdc3c6cc859eed5ef552
Pantileev Danila <neeta7love@gmail.com>
2025-03-19 18:40:50 +0300

    Refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,30 @@ class Rcc {
         };
 };
 
-Rcc clock_system;
+//Rcc clock_system;
 
 //==============================================================================
 int main() {
 
     rcc_periph_clock_enable(RCC_GPIOD);
 
-    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
-    while (true) {
-        gpio_toggle(GPIOD, GPIO15);
-        for (volatile uint32_t i = 0; i<1'000'000; ++i);
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15 | GPIO13);
+
+    for (uint32_t ptime{0}; true; ++ptime){
+
+        gpio_toggle(GPIOD, GPIO13);
+
+        if (ptime % 1'000'000 < 500'000) {
+            gpio_set(GPIOD, GPIO15);
+        }
+        else {
+            gpio_clear(GPIOD, GPIO15);
+        }
+        
     }
+            
+    // while (true) {
+    //     gpio_toggle(GPIOD, GPIO15);
+    //     for (volatile uint32_t i = 0; i<2'000'000; ++i);
+    // }
 }
