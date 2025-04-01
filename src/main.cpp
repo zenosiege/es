d78dc8908d6de974e047a8b32a0325eea1b6f559
Pantileev Danila <neeta7love@gmail.com>
2025-04-01 18:42:09 +0300

    Add tim6_dac_isr

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
+#include <libopencm3/cm3/nvic.h>
 //gpio - general purpose input-output
 
 int main() {
@@ -11,6 +12,10 @@ int main() {
     rcc_periph_clock_enable(RCC_TIM6);
     timer_set_prescaler(TIM6, 8000-1);
     timer_set_period(TIM6, 1000-1);
+
+    timer_enable_irq(TIM6, TIM_DIER_UIE);
+    nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
+
     timer_enable_counter(TIM6);
 
     while(true) {
@@ -22,4 +27,11 @@ int main() {
         }
     }
 
+}
+
+// реакция от процессора на сигнал
+
+void tim6_dac_isr (void){
+    gpio_toggle(GPIOD, GPIO15);
+    timer_clear_flag(TIM6, TIM_SR_UIF);
 }
