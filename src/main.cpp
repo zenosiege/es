50011e0450d264a8f9a189efb9b21e86935eb049
Pantileev Danila <neeta7love@gmail.com>
2025-04-01 18:16:25 +0300

    Working timer

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,23 @@
 #include <libopencm3/stm32/timer.h>
 //gpio - general purpose input-output
 
-class Rcc {
-    public:
-        Rcc() {
-            rcc_clock_setup_pll(&rcc_hse_16mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
-        };
-};
-
-//Rcc clock_system;
-
-//==============================================================================
 int main() {
 
     rcc_periph_clock_enable(RCC_GPIOD);
+    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
 
-    gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15 | GPIO13);
+    rcc_periph_clock_enable(RCC_TIM6);
+    timer_set_prescaler(TIM6, 8000-1);
+    timer_set_period(TIM6, 1000-1);
+    timer_enable_counter(TIM6);
 
-    for (uint32_t ptime{0}; true; ++ptime){
-
-        gpio_toggle(GPIOD, GPIO13);
-
-        if (ptime % 1'000'000 < 500'000) {
+    while(true) {
+        if(timer_get_counter(TIM6) < 800){
             gpio_set(GPIOD, GPIO15);
         }
         else {
             gpio_clear(GPIOD, GPIO15);
         }
-        
     }
-            
-    // while (true) {
-    //     gpio_toggle(GPIOD, GPIO15);
-    //     for (volatile uint32_t i = 0; i<2'000'000; ++i);
-    // }
+
 }
